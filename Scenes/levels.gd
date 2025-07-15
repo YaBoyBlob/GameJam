98aabc57386739142b97e15dd9da4221edb4550c
YaBoyBlob <0802jason@gmail.com>
2025-07-15 22:48:02 +0800

    update

--- Scenes/levels.gd
+++ Scenes/levels.gd
@@ -3,27 +3,39 @@ extends Node2D
 @onready var level_camera = $LevelCamera
 @onready var player = $MainCharacter
 
+@onready var block = $CanvasLayer/Block
+@onready var ball = $CanvasLayer/Ball
+@onready var plank = $CanvasLayer/Plank
+@onready var reset = $CanvasLayer/Reset
+
+var objects = 0
+var level = 1
 var box = preload("res://Units/block.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	blink.play("Blink-Open")
 
-var level = 1
+
 
 func _on_area_2d_area_exited(area):
 	if level > 0:
 		level_camera.global_position.x += 640
 	level +=1
+	match level:
+		2:
+			block.show()
 
 
-
-func _on_reset_button_down():
+func _on_reset_button_down(): #reset everything
 	player.global_position = level_camera.global_position - Vector2(240,0)
-	player.velocity.x = 0
-	player.velocity.y = 0
-
-
-func _on_box_button_down():
-	var instance = box.instantiate()
-	add_child(instance)
-	instance.position = player.global_position + Vector2(50 * player.prev_direction , 0)
+	player.velocity = Vector2.ZERO
+	for nodes in get_tree().get_nodes_in_group("Objects"):
+		nodes.queue_free()
+	objects = 0
+	
+func _on_box_button_down(): #spawn blocks
+	if objects <3:
+		var instance = box.instantiate()
+		add_child(instance)
+		instance.position = player.global_position + Vector2(50 * player.prev_direction , 0)
+		objects += 1
